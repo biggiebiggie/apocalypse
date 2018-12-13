1d2bbba18dbd8928b6c144a99a061bd327e74820
Magnus stampe Nielsen <magnusstampenielsen@laptop-rqfl2044.kea.local>
2018-12-13 10:08:42 +0100

    donation.html styled and minor fixes

--- database.js
+++ database.js
@@ -17,7 +17,6 @@ firebase.initializeApp(config);
 const txtEmail = document.querySelector("#txtEmail");
 const txtPassword = document.querySelector("#txtPassword");
 const btnLogin = document.querySelector("#btnLogin");
-const btnSignup = document.querySelector("#btnSignup");
 const btnLogout = document.querySelector("#btnLogout");
 
 let firebasesAuthDatabaseID = false;
@@ -32,44 +31,44 @@ btnLogin.addEventListener("click", e => {
   promise.catch(e => console.log(e.message));
 });
 
-//Sign up button
-btnSignup.addEventListener("click", e => {
-  //get info
-  const email = txtEmail.value;
-  const password = txtPassword.value;
-  const auth = firebase.auth();
-  //createuser
-  // const promise = auth.createUserWithEmailAndPassword(email, password);
-  // promise.catch(e => console.log(e.message));
+// //Sign up button
+// btnSignup.addEventListener("click", e => {
+//   //get info
+//   const email = txtEmail.value;
+//   const password = txtPassword.value;
+//   const auth = firebase.auth();
+//   //createuser
+//   // const promise = auth.createUserWithEmailAndPassword(email, password);
+//   // promise.catch(e => console.log(e.message));
 
-  //Authentication of user
-  auth
-    .createUserWithEmailAndPassword(email, password)
-    .then(authData => {
-      console.log("User created successfully with payload-", authData);
-      //Write code to use authData to add to Users
-      firebase.database().ref("userinfo/").push({
-        email: email,
-        uid: authData.user.uid,
-        donations: {
-          amount: 0,
-          materials: {
-            wood: 0,
-            cement: 0,
-            miscellaneous: 0,
-            clothes: 0
-          },
-          food: {
-            water: 0,
-            MRE: 0
-          }
-        }
-      });
-    })
-    .catch(_error => {
-      console.log("Login Failed!", _error);
-    });
-});
+//   //Authentication of user
+//   auth
+//     .createUserWithEmailAndPassword(email, password)
+//     .then(authData => {
+//       console.log("User created successfully with payload-", authData);
+//       //Write code to use authData to add to Users
+//       firebase.database().ref("userinfo/").push({
+//         email: email,
+//         uid: authData.user.uid,
+//         donations: {
+//           amount: 0,
+//           materials: {
+//             wood: 0,
+//             cement: 0,
+//             miscellaneous: 0,
+//             clothes: 0
+//           },
+//           food: {
+//             water: 0,
+//             MRE: 0
+//           }
+//         }
+//       });
+//     })
+//     .catch(_error => {
+//       console.log("Login Failed!", _error);
+//     });
+// });
 
 //Logout button
 btnLogout.addEventListener("click", e => {
@@ -125,7 +124,10 @@ function findFirebaseUser(email) {
 
 const userList = document.querySelector("#user_list");
 // Create ref
-const DBRefUserInfo = firebase.database().ref().child("userinfo");
+const DBRefUserInfo = firebase
+  .database()
+  .ref()
+  .child("userinfo");
 
 DBRefUserInfo.on("child_added", snap => {
   if (userList) {
